0c4c746ab9b6bdafedfc8ef269b22ea728b90317
sujata <gunalesujata@gmail.com>
2020-09-24 22:34:14 +0530

    reducer creation

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import CartContainer from "./Components/CartContainer";
 
+import { createStore } from "redux";
+import reducer from "./Container/Reducer";
+import { Provider } from "react-redux";
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Navbar />
       <CartContainer />
-    </>
+    </Provider>
   );
 }
 
