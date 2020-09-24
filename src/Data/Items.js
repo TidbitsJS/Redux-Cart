29bdfe54424d62b810cc4c361894cbb3d75f917e
sujata <gunalesujata@gmail.com>
2020-09-24 14:11:48 +0530

    cart item data

---
+++ src/Data/Items.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    title: "Samsung Galaxy S12",
+    price: 799.99,
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368215/phone-2_ohtt5s.png",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "Google Pixel",
+    price: 505.05,
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583371867/phone-1_gvesln.png",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Xiaomi Redmi Note 10",
+    price: 999.99,
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368224/phone-3_h2s6fo.png",
+    amount: 1,
+  },
+];
